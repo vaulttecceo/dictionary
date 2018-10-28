20781e5c5f34c18e14d6929e45be7bf96390173e
TheNightisShort <mickeyjoh@yahoo.com>
2018-10-27 20:43:57 -0500

    i did it

--- src/src/test.java
+++ src/src/test.java
@@ -1,10 +1,19 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class test {
-    public static void main(String[] args) {
-        System.out.print("hi john ");
-        System.out.println("hi erik");
-        //nice
+    public static void main(String[] args) throws IOException {
+        String file = "/Users/johnmcguinness/Desktop/JavaPrograms/dictwitherik/src/src/source.txt";
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String currentLine = reader.readLine();
+        reader.close();
+
+        System.out.println(currentLine);
 
     }
+
 
 
 }
