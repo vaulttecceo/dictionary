691a03b65c928d9d2c658a46608b2901a54620d8
TheNightisShort <mickeyjoh@yahoo.com>
2018-10-28 19:20:48 -0500

    halp

--- src/src/test.java
+++ src/src/test.java
@@ -1,19 +1,40 @@
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.Scanner;
+import java.io.*;
+import java.util.Map;
+
 
 public class test {
+    static char[] buff = new char[3];
+    String key, value;
+   static boolean onKey = true;
+
     public static void main(String[] args) throws IOException {
-        String file = "/Users/johnmcguinness/Desktop/JavaPrograms/dictwitherik/src/src/source.txt";
+        File file = new File("/Users/johnmcguinness/Desktop/JavaPrograms/dictwitherik/src/src/source.txt");
+        for(int i = 0; 0 < file.length(); i++){
+            lookingAtFile(file);
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String currentLine = reader.readLine();
-        reader.close();
+        }
+    }
+    public static void lookingAtFile(File f) throws IOException {
+        Scanner in = new Scanner(f);
+        while(in.hasNext()){
+           for(int j = 0; j < f.length(); j++){
+               if(onKey){
+                  buff[] = in.next().charAt(j);
 
-        System.out.println(currentLine);
+               }
+               if(buff.equals("\";\"") && onKey==false){
+                   onKey = true;
+               }
+               if(buff.equals("\",\"") && onKey){
+                   onKey = false;
+               }
+           }
+
+       }
 
     }
-
-
 
 }
