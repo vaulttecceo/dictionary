15ee99239de1b722600281f54e74866863342cf3
vaulttecceo <errodv@live.com>
2018-10-29 19:57:53 -0500

    nice

--- src/test.java
+++ src/test.java
@@ -6,12 +6,26 @@ import java.util.Stack;
 
 public class test {
 
+    tempMap map = new tempMap();
+    char buf[] = new char[3];
+
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\Guadalupe\\IdeaProjects\\dictionary\\src\\src\\source.txt");
-
+        Stack s = new Stack<String>(); //fill s with each character from the file in sequential order
+        mapGenerator(s);
         }
 
-    /*public static void lookingAtFile(File f) throws IOException {
+
+    public static void mapGenerator(Stack<String> stack){
+        String key, value;
+        boolean onKey = true;
+
+        for(String c : stack)
+            // do stuff here
+            
+    }
+}
+  /*public static void lookingAtFile(File f) throws IOException {
         Scanner in = new Scanner(f);
         while(in.hasNext()){
                if(buff.equals("\";\"") && onKey==false){
@@ -28,14 +42,3 @@ public class test {
        }
 //            https://docs.google.com/document/d/12j9RDOletLUL42Sn_BffExvSUenzcRN2_LlqooTqYg8/edit?usp=sharing
     }*/
-
-    void mapGenerator(Stack<String> stack){
-        char currentChar;
-        char buf[] = new char[3];
-        String key, value;
-        boolean onKey = true;
-        Map map = new Map<String, String>();
-
-
-    }
-}
